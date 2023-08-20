f8f36f6b0968916513c4c342b5e5fff515582a66
badr mohammed <badralmutairi111@gmail.com>
2023-08-20 16:20:39 +0300

    .

--- sogoapi/settings.py
+++ sogoapi/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
+
 MEDIA_ROOT = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 # Default primary key field type
@@ -142,5 +145,3 @@ REST_FRAMEWORK = {
     ],
 }
 
-STATICFILES_DIRS = [BASE_DIR / "static/"] # new
-STATIC_ROOT = BASE_DIR / "staticfiles" # new
