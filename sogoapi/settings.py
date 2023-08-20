a107ee1075d057108eaa68803671a68e7030cccd
badr mohammed <badralmutairi111@gmail.com>
2023-08-20 15:43:37 +0300

    .

--- sogoapi/settings.py
+++ sogoapi/settings.py
@@ -128,9 +128,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+# ]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
